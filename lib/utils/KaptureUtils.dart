674162dcc9709c55edc6e5ddac3cb0b33c182a3d
kirtimancs19 <kirtiman.singh@kapturecrm.com>
2024-10-12 15:43:07 +0530

    add route path

--- lib/utils/KaptureUtils.dart
+++ lib/utils/KaptureUtils.dart
@@ -6,13 +6,9 @@ import 'package:flutter_kapture_ticketing_sdk/flutter_kapture_ticketing_sdk.dart
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class KaptureUtils {
+  static final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
-  static final FlutterLocalNotificationsPlugin
-      _flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
-
-  static void initializeKaptureNotifications(
-      // Listen for local notification taps
-      GlobalKey<NavigatorState> navigationKey) async {
+  static void initializeKaptureNotifications(GlobalKey<NavigatorState> navigationKey, String? route, String? baseUrl) async {
     NotificationAppLaunchDetails? notificationResponse =
         await _flutterLocalNotificationsPlugin
             .getNotificationAppLaunchDetails();
@@ -36,13 +32,13 @@ class KaptureUtils {
       initializationSettings,
       onDidReceiveNotificationResponse: (NotificationResponse response) async {
         // Handle notification tap
-        _handleKaptureNotification(response, navigationKey);
+        _handleKaptureNotification(response, navigationKey, route, baseUrl);
       },
     );
 
     if (payload != null) {
       Timer(const Duration(seconds: 3), () {
-        _handleKaptureNotification(payload, navigationKey);
+        _handleKaptureNotification(payload, navigationKey, route, baseUrl);
       });
     }
   }
@@ -74,59 +70,76 @@ class KaptureUtils {
           message.data["title"],
           message.data["body"],
           notificationDetails,
-          payload: message.data.toString(),
+          payload: jsonEncode(message.data),
         );
-      } else{
+        return true;
+      }
+      else if(message.data.containsKey("message")){
+        Map<String, dynamic> messageObj = jsonDecode(message.data["message"]);
+        if(messageObj["task"]["taskTitle"]!=null) {
+          await _flutterLocalNotificationsPlugin.show(
+            0,
+            messageObj["task"]["taskTitle"],
+            "",
+            notificationDetails,
+            payload: jsonEncode(message.data),
+          );
+          return true;
+        }
+      }
+      else if (message.data.containsKey("notification")) {
+        Map<String, dynamic> notification = jsonDecode(message.data["notification"]);
+        Map<String, dynamic> parsedData = {
+          ...message.data,
+          "notification": notification,
+        };
+        print("Notification Data: $parsedData");
         return false;
       }
-      return true;
+      return false;
     }
     return false;
   }
 
-  static Future<void> _handleKaptureNotification(NotificationResponse message,
-    GlobalKey<NavigatorState> navigationKey) async {
-    try {
-      String fixedJsonString = KaptureUtils().fixPayload(message.payload??'{}');
-      Map<String, dynamic> response = jsonDecode(fixedJsonString);
-      navigationKey.currentState?.pushNamed('/kapture', arguments: {
-        'url':(response.containsKey("url") && response["url"]!=null)?response["url"]:"https://goldenrama.kapturecrm.com/nui_develop/qk_2",
-      },);
-    } catch(error) {}
-  }
-
-  String fixPayload(String payload) {
-    // Use regex to fix the payload format
-    String fixedJson = payload.replaceAllMapped(
-      RegExp(r'(\w+)\s*:\s*("[^"]*"|[^,\s}]+(?:\s+[^,\s}]+)*)'),
-          (match) {
-        String key = '"${match[1]}"'; // Wrap key in quotes
-        String value = match[2].toString(); // Get the value
-
-        // Check if the value is not quoted and wrap it in quotes if necessary
-        if (!value.startsWith('"')) {
-          value = '"$value"'; // Add quotes around non-quoted values
+  static Future<void> _handleKaptureNotification(NotificationResponse message, GlobalKey<NavigatorState> navigationKey, String? route, String? baseUrl) async {
+    if(route!=null && baseUrl!=null) {
+      try {
+        Map<String, dynamic> response = jsonDecode(message.payload ?? "");
+        if (response.containsKey("message")) {
+          var messageObj = jsonDecode(response["message"]);
+          if (messageObj["task"]["ticketId"] != null && messageObj["task"]["ticketId"] != "") {
+            navigationKey.currentState?.pushNamed(route, arguments: {
+              'url': baseUrl + "/tickets/assigned_to_me/5/-1/0/detail/" +
+                  messageObj["task"]["ticketId"],
+            },);
+          } else {
+            navigationKey.currentState?.pushNamed(route, arguments: {
+              'url': baseUrl,
+            },);
+          }
         }
-
-        return '$key: $value'; // Return the formatted key-value pair
-      },
-    );
-
-    // Ensure the string is properly closed with a brace
-    return fixedJson.trim().endsWith('}') ? fixedJson : '$fixedJson}';
-  }
-
-  static Map<String, WidgetBuilder> getRoutes() {
-    return {
-      '/kapture': (context) {
-        // Extract the arguments from the route settings
-        final args = ModalRoute
-            .of(context)
-            ?.settings
-            .arguments as Map<String, dynamic>;
-        // Return the widget with the extracted arguments
-        return KapturePackage(url: args.containsKey("url") ? args['url']: "https://goldenrama.kapturecrm.com/nui_develop/qk_2", fcmToken: args.containsKey("fcmToken")?args['fcmToken']:"",);
+        else if (response.containsKey("notification")) {
+          navigationKey.currentState?.pushNamed(route, arguments: {
+            'url': baseUrl,
+          },);
+        }
+        else if (response.containsKey("url")) {
+          navigationKey.currentState?.pushNamed(route, arguments: {
+            'url': (response.containsKey("url") && response["url"] != null)
+                ? response["url"]
+                : baseUrl,
+          },);
+        }
+        else {
+          navigationKey.currentState?.pushNamed(route, arguments: {
+            'url': baseUrl,
+          },);
+        }
+      } catch (error) {
+        navigationKey.currentState?.pushNamed(route, arguments: {
+          'url': baseUrl,
+        },);
       }
-    };
+    }
   }
 }
